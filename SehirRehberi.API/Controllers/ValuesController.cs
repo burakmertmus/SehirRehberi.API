8659a9edadfd712b9683bd95a52a3145536af957
Burak Mert Muş <burakmertmus@hotmail.com>
2021-06-29 02:23:23 +0300

    Add project files.

---
+++ SehirRehberi.API/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SehirRehberi.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ValuesController : ControllerBase
+    {
+
+        private DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(v=>v.Id==id);
+            return Ok(value);
+
+        }
+    }
+}
